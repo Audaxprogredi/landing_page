3a8f03851b07e0d26d711a45220ced719311e7cb
José Iván Salazar <josesala@espol.edu.ec>
2026-06-08 19:09:08 +0000

    toast

--- src/js/file01.js
+++ src/js/file01.js
@@ -1,6 +1,12 @@
 "use strict";
 
+const showToast = () => {
+    const toast = document.getElementById("toast-interactive");
+    if (toast) {
+        toast.classList.add("md:block");
+    }
+};
+
 (() => {
-    alert("¡Bienvenido a la página!");
-    console.log("Mensaje de bienvenida mostrado.");
+    showToast();
 })();
