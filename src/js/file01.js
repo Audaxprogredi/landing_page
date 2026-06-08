29bdc70b7877bc168b6762b560b7601e6293777c
José Iván Salazar <josesala@espol.edu.ec>
2026-06-08 19:19:48 +0000

    rickroolll

--- src/js/file01.js
+++ src/js/file01.js
@@ -4,9 +4,21 @@ const showToast = () => {
     const toast = document.getElementById("toast-interactive");
     if (toast) {
         toast.classList.add("md:block");
+        toast.classList.add("hidden");
+
+    }
+};
+
+const showVideo = () => {
+    const demo = document.getElementById("demo");
+    if (demo) {
+        demo.addEventListener("click", () => {
+            window.open("https://dawm2.readthedocs.io/es/latest/guias/guia08.html", "_blank");
+        });
     }
 };
 
 (() => {
     showToast();
+    showVideo();
 })();
